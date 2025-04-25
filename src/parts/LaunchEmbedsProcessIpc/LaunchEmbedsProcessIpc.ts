650f8b5c3327d5eb57d73e6a766abf594960afbb
Le Vivilet <levivlet@pm.me>
2025-04-25 21:06:11 +0200

    feature: improve error message when launching embeds process fails

--- src/parts/LaunchEmbedsProcessIpc/LaunchEmbedsProcessIpc.ts
+++ src/parts/LaunchEmbedsProcessIpc/LaunchEmbedsProcessIpc.ts
@@ -1,10 +1,15 @@
 import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
 import * as IpcParent from '../IpcParent/IpcParent.ts'
+import { VError } from '@lvce-editor/verror'
 
 export const launchEmbedsProcessIpc = async () => {
-  const ipc = await IpcParent.create({
-    initialCommand: 'HandleMessagePortForEmbedsProcess.handleMessagePortForEmbedsProcess',
-  })
-  HandleIpc.handleIpc(ipc)
-  return ipc
+  try {
+    const ipc = await IpcParent.create({
+      initialCommand: 'HandleMessagePortForEmbedsProcess.handleMessagePortForEmbedsProcess',
+    })
+    HandleIpc.handleIpc(ipc)
+    return ipc
+  } catch (error) {
+    throw new VError(error, `Failed to launch embeds process`)
+  }
 }
