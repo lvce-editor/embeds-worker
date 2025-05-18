6fc1d568cf1d8d9ae56d48bfda54e4fcf6d3ffdf
Le Vivilet <levivlet@pm.me>
2025-05-18 12:40:40 +0200

    feature: add function to create embeds process rpc

---
+++ src/parts/CreateEmbedsProcessRpc/CreateEmbedsProcessRpc.ts
@@ -0,0 +1,21 @@
+import { type Rpc, MessagePortRpcParent } from '@lvce-editor/rpc'
+import { VError } from '@lvce-editor/verror'
+import * as GetPortTuple from '../GetPortTuple/GetPortTuple.ts'
+import { sendMessagePortToEmbedsProcess } from '../SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts'
+
+export const createEmbedsProcessRpc = async (): Promise<Rpc> => {
+  try {
+    const { port1, port2 } = GetPortTuple.getPortTuple()
+    await sendMessagePortToEmbedsProcess(port2)
+    port1.start()
+    const rpc = await MessagePortRpcParent.create({
+      commandMap: {},
+      messagePort: port1,
+      isMessagePortOpen: false,
+    })
+    // TODO createMessageportRpcParent should call port start
+    return rpc
+  } catch (error) {
+    throw new VError(error, `Failed to create embeds process rpc`)
+  }
+}
