603ff84fff9547e7655769b4acacd94d943c62e4
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-18 12:56:36 +0200

    feature: simplify rpc (#39)
    * feature: simplify rpc
    
    * tsc

--- src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
+++ src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
@@ -3,6 +3,7 @@ import * as Rpc from '../ParentRpc/ParentRpc.ts'
 import * as LoadErrorCode from '../LoadErrorCode/LoadErrorCode.ts'
 
 export const createWebContentsView = async (restoreId, fallThroughKeyBindings) => {
+  // @ts-ignore
   const id = await EmbedsProcess.invoke('ElectronWebContentsView.createWebContentsView', restoreId, fallThroughKeyBindings)
   return id
 }
@@ -12,16 +13,19 @@ export const disposeWebContentsView = (id) => {
 }
 
 export const resizeWebContentsView = (id, x, y, width, height) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.resizeBrowserView', id, x, y, width, height)
 }
 
 export const setIframeSrcFallback = async (id, error) => {
   const { code, message } = error
+  // @ts-ignore
   await EmbedsProcess.invoke('ElectronWebContentsView.setIframeSrcFallback', id, code, message)
 }
 
 export const setIframeSrc = async (id, iframeSrc) => {
   try {
+    // @ts-ignore
     await EmbedsProcess.invoke('ElectronWebContentsView.setIframeSrc', id, iframeSrc)
   } catch (error) {
     console.log({ error })
@@ -47,54 +51,67 @@ export const setIframeSrc = async (id, iframeSrc) => {
 }
 
 export const focus = (id) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.focus', id)
 }
 
 export const openDevtools = (id) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.openDevtools', id)
 }
 
 export const reload = (id) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.reload', id)
 }
 
 export const show = (id) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.show', id)
 }
 
 export const hide = (id) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.hide', id)
 }
 
 export const forward = (id) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.forward', id)
 }
 
 export const backward = (id) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.backward', id)
 }
 
 export const getDomTree = (id) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.getDomTree', id)
 }
 
 export const insertCss = (id, css) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.insertCss', id, css)
 }
 
 export const insertJavaScript = (id, code) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.insertJavaScript', id, code)
 }
 
 export const cancelNavigation = (id) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.cancelNavigation', id)
 }
 
 export const inspectElement = (id, x, y) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.inspectElement', id, x, y)
 }
 
 export const copyImageAt = (id, x, y) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.copyImageAt', id, x, y)
 }
 
@@ -104,6 +121,7 @@ export const setFallthroughKeyBindings = (id, fallthroughKeybindings) => {
 }
 
 export const getStats = (id, fallthroughKeybindings) => {
+  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.getStats', id, fallthroughKeybindings)
 }
 
--- src/parts/EmbedsProcess/EmbedsProcess.ts
+++ src/parts/EmbedsProcess/EmbedsProcess.ts
@@ -1,28 +1,3 @@
-import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
-import * as LaunchEmbedsProcessIpc from '../LaunchEmbedsProcessIpc/LaunchEmbedsProcessIpc.ts'
 import { EmbedsProcess } from '@lvce-editor/rpc-registry'
 
-interface State {
-  workerPromise?: Promise<any>
-}
-
-const state: State = {
-  workerPromise: undefined,
-}
-
-export const getOrCreate = () => {
-  state.workerPromise ||= LaunchEmbedsProcessIpc.launchEmbedsProcessIpc()
-  return state.workerPromise
-}
-
-export const invokeAndTransfer = async (transfer, method, ...params) => {
-  const ipc = await getOrCreate()
-  return JsonRpc.invokeAndTransfer(ipc, transfer, method, ...params)
-}
-
-export const invoke = async (method, ...params) => {
-  const ipc = await getOrCreate()
-  return JsonRpc.invoke(ipc, method, ...params)
-}
-
-export const { set } = EmbedsProcess
+export const { set, invoke, invokeAndTransfer } = EmbedsProcess
