cd632836ecf43b119583dbff80f019b7df272b76
Le Vivilet <levivlet@pm.me>
2025-05-21 21:07:39 +0200

    feature: remove unused ts ignore

--- packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
+++ packages/embeds-worker/src/parts/ElectronWebContentsView/ElectronWebContentsView.ts
@@ -13,29 +13,29 @@ export const disposeWebContentsView = (id: string): Promise<void> => {
 }
 
 export const resizeWebContentsView = (id: string, x: number, y: number, width: number, height: number): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.resizeBrowserView', id, x, y, width, height)
 }
 
 export const setIframeSrcFallback = async (id: string, error: any): Promise<void> => {
   const { code, message } = error
-  // @ts-ignore
+
   await EmbedsProcess.invoke('ElectronWebContentsView.setIframeSrcFallback', id, code, message)
 }
 
 export const setIframeSrc = async (id: string, iframeSrc: string): Promise<void> => {
   try {
-    // @ts-ignore
     await EmbedsProcess.invoke('ElectronWebContentsView.setIframeSrc', id, iframeSrc)
   } catch (error) {
     console.log({ error })
     // TODO send error back to embeds worker,
     // embeds worker decides how to handle error
+
     // @ts-ignore
     if (error && error.code === LoadErrorCode.ERR_ABORTED) {
       console.info(`[embeds worker] navigation to ${iframeSrc} aborted`)
       return
     }
+
     // @ts-ignore
     if (error && error.code === LoadErrorCode.ERR_FAILED) {
       console.info(`[embeds worker] navigation to ${iframeSrc} canceled`)
@@ -51,67 +51,54 @@ export const setIframeSrc = async (id: string, iframeSrc: string): Promise<void>
 }
 
 export const focus = (id: string): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.focus', id)
 }
 
 export const openDevtools = (id: string): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.openDevtools', id)
 }
 
 export const reload = (id: string): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.reload', id)
 }
 
 export const show = (id: string): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.show', id)
 }
 
 export const hide = (id: string): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.hide', id)
 }
 
 export const forward = (id: string): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.forward', id)
 }
 
 export const backward = (id: string): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.backward', id)
 }
 
 export const getDomTree = (id: string): Promise<any> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.getDomTree', id)
 }
 
 export const insertCss = (id: string, css: string): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.insertCss', id, css)
 }
 
 export const insertJavaScript = (id: string, code: string): Promise<any> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.insertJavaScript', id, code)
 }
 
 export const cancelNavigation = (id: string): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.cancelNavigation', id)
 }
 
 export const inspectElement = (id: string, x: number, y: number): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.inspectElement', id, x, y)
 }
 
 export const copyImageAt = (id: string, x: number, y: number): Promise<void> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.copyImageAt', id, x, y)
 }
 
@@ -121,7 +108,6 @@ export const setFallthroughKeyBindings = async (id: string, fallthroughKeybindin
 }
 
 export const getStats = (id: string, fallthroughKeybindings: any): Promise<any> => {
-  // @ts-ignore
   return EmbedsProcess.invoke('ElectronWebContentsView.getStats', id, fallthroughKeybindings)
 }
 
