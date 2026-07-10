d09e6def905fc48940c41fb7e92d16c4b1a3e491
Le Vivilet <levivilet@pm.me>
2026-07-10 15:01:53 +0200

    feature: update eslint config (#98)

--- packages/embeds-worker/test/SendMessagePortToEmbedsProcess.test.ts
+++ packages/embeds-worker/test/SendMessagePortToEmbedsProcess.test.ts
@@ -1,18 +1,22 @@
-import { test } from '@jest/globals'
+import { expect, test, jest } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
 import * as ParentRpc from '../src/parts/ParentRpc/ParentRpc.ts'
 import * as SendMessagePortToEmbedsProcess from '../src/parts/SendMessagePortToEmbedsProcess/SendMessagePortToEmbedsProcess.ts'
 
 test('sendMessagePortToEmbedsProcess sends port to embeds process', async () => {
+  const invoke = jest.fn()
+  const invokeAndTransfer = jest.fn()
   const mockRpc = MockRpc.create({
     commandMap: {},
     invoke: (method: string) => {
+      invoke(method)
       if (method === 'SendMessagePortToExtensionHostWorker.sendMessagePortToSharedProcess') {
         return Promise.resolve()
       }
       throw new Error(`unexpected method ${method}`)
     },
-    invokeAndTransfer: (method: string) => {
+    invokeAndTransfer: (method: string, ...args: readonly unknown[]) => {
+      invokeAndTransfer(method, ...args)
       if (method === 'SendMessagePortToExtensionHostWorker.sendMessagePortToSharedProcess') {
         return Promise.resolve()
       }
@@ -22,4 +26,13 @@ test('sendMessagePortToEmbedsProcess sends port to embeds process', async () => 
   ParentRpc.set(mockRpc)
   const { port1 } = new MessageChannel()
   await SendMessagePortToEmbedsProcess.sendMessagePortToEmbedsProcess(port1)
+
+  expect(invoke).toHaveBeenCalledTimes(0)
+  expect(invokeAndTransfer).toHaveBeenCalledTimes(1)
+  expect(invokeAndTransfer).toHaveBeenCalledWith(
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToSharedProcess',
+    port1,
+    'HandleMessagePortForEmbedsProcess.handleMessagePortForEmbedsProcess',
+    208,
+  )
 })
