a580c0040be0a7117a2c9414873e3c804f854ee7
Le Vivilet <levivilet@pm.me>
2026-07-30 08:26:12 -0700

    feature: add knip (#110)

--- packages/embeds-worker/src/parts/EmbedsProcess/EmbedsProcess.ts
+++ packages/embeds-worker/src/parts/EmbedsProcess/EmbedsProcess.ts
@@ -1,5 +1,5 @@
 import { EmbedsProcess } from '@lvce-editor/rpc-registry'
 
-export const { invoke, invokeAndTransfer, set } = EmbedsProcess
+export const { invoke, set } = EmbedsProcess
 
 export const invokeAny = invoke as (method: string, ...params: readonly any[]) => Promise<any>
