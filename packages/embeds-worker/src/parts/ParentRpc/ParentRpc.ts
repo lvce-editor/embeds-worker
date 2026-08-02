cdc6f1326fbe466bff9e77433210c35a70cd7ca5
Le Vivilet <levivilet@pm.me>
2026-08-02 02:47:05 +0000

    feature: update eslint dependencies

--- packages/embeds-worker/src/parts/ParentRpc/ParentRpc.ts
+++ packages/embeds-worker/src/parts/ParentRpc/ParentRpc.ts
@@ -1,5 +1,14 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { RendererWorker, type RendererWorkerApi } from '@lvce-editor/rpc-registry'
 
-export const { invoke, invokeAndTransfer, set } = RendererWorker
+export const invoke = <T extends keyof RendererWorkerApi>(method: T, ...params: Parameters<RendererWorkerApi[T]>): ReturnType<RendererWorkerApi[T]> =>
+  RendererWorker.invoke(method, ...params)
 
-export const invokeAny = invoke as (method: string, ...params: readonly any[]) => Promise<any>
+export const invokeAndTransfer = <T extends keyof RendererWorkerApi>(
+  method: T,
+  ...params: Parameters<RendererWorkerApi[T]>
+): ReturnType<RendererWorkerApi[T]> => RendererWorker.invokeAndTransfer(method, ...params)
+
+export const set = (...args: Readonly<Parameters<typeof RendererWorker.set>>): ReturnType<typeof RendererWorker.set> => RendererWorker.set(...args)
+
+export const invokeAny = (method: string, ...params: readonly any[]): Promise<any> =>
+  (RendererWorker.invoke as (method: string, ...params: readonly any[]) => Promise<any>)(method, ...params)
