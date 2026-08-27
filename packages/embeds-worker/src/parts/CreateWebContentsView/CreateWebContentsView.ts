1eb488f30f249eb71ac83cac9d76e2da3a68ec5d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2026-08-27 09:58:48 +0000

    fix: restore web contents view arguments

--- packages/embeds-worker/src/parts/CreateWebContentsView/CreateWebContentsView.ts
+++
@@ -1,5 +0,0 @@
-import * as EmbedsProcess from '../EmbedsProcess/EmbedsProcess.ts'
-
-export const createWebContentsView = async (restoreId: number, ipcId: number): Promise<any> => {
-  return EmbedsProcess.invokeAny('ElectronWebContentsView.createWebContentsView', ipcId, restoreId)
-}
