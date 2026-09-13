14298000435094299503e471889e71b6f14ced69
Le Vivilet <levivilet@pm.me>
2026-09-13 22:53:20 +0200

    feature: migrate build tooling to TypeScript (#129)

--- packages/build/src/build-static.ts
+++ packages/build/src/build-static.ts
@@ -1,6 +1,6 @@
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
-import { root } from './root.js'
+import { root } from './root.ts'
 import { cp } from 'node:fs/promises'
 
 const sharedProcessPath = join(root, 'node_modules', '@lvce-editor', 'shared-process', 'index.js')
