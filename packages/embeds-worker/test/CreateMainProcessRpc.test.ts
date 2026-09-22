504db515fcf2a677c730d10673048b0f68246dec
Le Vivilet <levivilet@pm.me>
2026-09-22 14:05:36 -0700

    fix: use browser message port rpc client (#135)
    * fix: use browser message port rpc client
    
    * test: cover main process rpc client
    
    * test: assert main process transport handoff

---
+++ packages/embeds-worker/test/CreateMainProcessRpc.test.ts
@@ -0,0 +1,29 @@
+import { afterEach, expect, jest, test } from '@jest/globals'
+import { MessagePortRpcClient } from '@lvce-editor/rpc'
+import { createMainProcessRpc } from '../src/parts/CreateMainProcessRpc/CreateMainProcessRpc.ts'
+import * as ParentRpc from '../src/parts/ParentRpc/ParentRpc.ts'
+
+const create = jest.spyOn(MessagePortRpcClient as any, 'create').mockImplementation(async () => ({}))
+const invokeAndTransfer = jest.fn(async (..._args: readonly unknown[]) => undefined)
+
+afterEach(() => {
+  create.mockClear()
+  invokeAndTransfer.mockClear()
+})
+
+test('createMainProcessRpc uses the browser message port rpc client', async () => {
+  ParentRpc.set({ invokeAndTransfer } as any)
+
+  await createMainProcessRpc(7)
+
+  expect(create).toHaveBeenCalledWith({
+    commandMap: expect.any(Object),
+    messagePort: expect.any(MessagePort),
+  })
+  expect(invokeAndTransfer).toHaveBeenCalledWith(
+    'SendMessagePortToMainProcess.sendMessagePortToMainProcess',
+    expect.any(MessagePort),
+    'ElectronWebContentsView.handleMessagePort',
+    1007,
+  )
+})
